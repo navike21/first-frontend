373a2d605c7964508931a7c2a008ea85aa9ae07a
José Iván Chapoñán Casiano <ivan@navike21.com>
2025-01-25 23:49:09 -0500

    chore: Update testing configuration and dependencies

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,23 @@
 import '@testing-library/jest-dom'
+import { cleanup } from '@testing-library/react'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+
+// Limpia automáticamente después de cada test
+afterEach(() => {
+  cleanup()
+})
+
+// Silencia los errores específicos de React si es necesario
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
+})
